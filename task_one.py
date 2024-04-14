336bd0d945f44aa212b13d28b048215a16510f1b
nnzakh10 <nnzakh10@mts.ru>
2024-04-14 17:33:14 +0300

    added test_add_group

--- task_one.py
+++ task_one.py
@@ -1,49 +1,54 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.implicitly_wait(30)
-        self.verificationErrors = []
-        self.accept_next_alert = True
 
-    def test_untitled_test_case(self):
-        driver = self.driver
-        driver.get("https://demoqa.com/")
-        driver.find_element_by_xpath("//div[h5='Forms']").click()
-        driver.find_element_by_xpath("(//*[normalize-space(text()) and normalize-space(.)='Forms'])[1]/following::span[1]").click()
-        driver.find_element_by_id("firstName").click()
-        driver.find_element_by_id("firstName").clear()
-        driver.find_element_by_id("firstName").send_keys(u"Тест")
-        driver.find_element_by_xpath("//div[@id='genterWrapper']/div[2]/div/label").click()
-        driver.find_element_by_id("submit").click()
+class TaskOne(unittest.TestCase):
+    def setUp(self):
+        self.wd = WebDriver()
+        self.wd.implicitly_wait(30)
+
+    def test_add_group(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
+        wd.find_element(By.NAME, "user").click()
+        wd.find_element(By.NAME, "user").clear()
+        wd.find_element(By.NAME, "user").send_keys("admin")
+        wd.find_element(By.NAME, "pass").click()
+        wd.find_element(By.NAME, "pass").clear()
+        wd.find_element(By.NAME, "pass").send_keys("secret")
+        wd.find_element(By.ID, "LoginForm").submit()
+        wd.find_element(By.LINK_TEXT, "groups").click()
+        wd.find_element(By.NAME, "new").click()
+        wd.find_element(By.NAME, "group_name").click()
+        wd.find_element(By.NAME, "group_name").clear()
+        wd.find_element(By.NAME, "group_name").send_keys("test")
+        wd.find_element(By.NAME, "group_header").click()
+        wd.find_element(By.NAME, "group_header").clear()
+        wd.find_element(By.NAME, "group_header").send_keys("test")
+        wd.find_element(By.NAME, "group_footer").click()
+        wd.find_element(By.NAME, "group_footer").clear()
+        wd.find_element(By.NAME, "group_footer").send_keys("test")
+        wd.find_element(By.NAME, "submit").click()
+        wd.find_element(By.LINK_TEXT, "group page").click()
+        wd.find_element(By.LINK_TEXT, "Logout").click()
 
     def is_element_present(self, how, what):
-        try: self.driver.find_element(how, what)
-        except NoSuchElementException as e: return False
+        try:
+            self.wd.find_element(how, what)
+        except NoSuchElementException as e:
+            return False
         return True
 
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
         return True
 
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-
     def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
-
+        self.wd.quit()
