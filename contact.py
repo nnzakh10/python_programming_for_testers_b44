8d0af16a85ba474beaefbfeefaae0f884be8f696
nnzakh10 <nnzakh10@mts.ru>
2024-05-02 10:08:41 +0300

    * Записал в рекордере сценарий для создания нового контакта в адресной книге * Перевел сценарий на язык Python и перенес его в среду разработки. * Выделил вспомогательные методы, чтобы повысить читаемость тестов. * Создал вспомогательный класс для хранения свойств контакта. * Использовал этот вспомогательный класс в тестах для инициализации тестовых данных и для передачи этих данных во вспомогательные методы.

---
+++ contact.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class Contact:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, fax,
+                 email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear):
+        self.ayear = ayear
+        self.amonth = amonth
+        self.aday = aday
+        self.byear = byear
+        self.bmonth = bmonth
+        self.bday = bday
+        self.homepage = homepage
+        self.email3 = email3
+        self.email2 = email2
+        self.email = email
+        self.fax = fax
+        self.work = work
+        self.mobile = mobile
+        self.home = home
+        self.address = address
+        self.company = company
+        self.title = title
+        self.nickname = nickname
+        self.lastname = lastname
+        self.middlename = middlename
+        self.firstname = firstname
